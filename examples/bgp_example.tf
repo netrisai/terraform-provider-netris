15e5aece6b7ee7da69dfee2a4de52406a5d44462
Artashes Balabekyan <artashes.balabekyan@netris.ai>
2021-09-16 14:33:40 +0400

    WIP: update example files

--- examples/bgp_example.tf
+++ examples/bgp_example.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    netris = {
-      source  = "netrisai/netris"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
-provider "netris" {
-  # address = ""                         # overwrite env: NETRIS_ADDRESS
-  # login = ""                           # overwrite env: NETRIS_LOGIN
-  # password = ""                        # overwrite env: NETRIS_PASSWORD
-}
-
 resource "netris_bgp" "artash-testt" {
   name = "artash-testt"
   site = "Yerevan"
