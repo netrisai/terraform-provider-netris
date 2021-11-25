6ac344c09fe95d575a3f69b0226c3daf037cc366
Gor Poghosyan <gor@netris.ai>
2021-11-25 21:48:44 +0400

    update ntpservers in inverntory_profile example

--- examples/inventory_profile_example.tf
+++ examples/inventory_profile_example.tf
@@ -4,7 +4,7 @@ resource "netris_inventory_profile" "my-profile" {
   ipv4ssh = ["10.0.10.0/24", "172.16.16.16"]
   ipv6ssh = ["2001:DB8::/32"]
   timezone = "America/Los_Angeles"
-  ntpservers = ["89.175.20.7", "91.206.8.34"]
+  ntpservers = ["0.pool.ntp.org", "132.163.96.5"]
   dnsservers = ["1.1.1.1", "8.8.8.8"]
   # customrule {
   #   sourcesubnet = "10.10.10.0/24"
