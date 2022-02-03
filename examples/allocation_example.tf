7e5c6ed27b31f1a7e2779d9ad81a50036d4743a7
Gor Poghosyan <gor@netris.ai>
2022-02-03 15:52:34 +0400

    fix main README; added doc for allocation

--- examples/allocation_example.tf
+++ examples/allocation_example.tf
@@ -6,25 +6,16 @@
 #   name = "my-allocation-mgmt"
 #   prefix = "192.0.2.0/24"
 #   tenantid = data.netris_tenant.admin.id
-#   depends_on = [
-#     netris_site.santa-clara,
-#   ]
 # }
 
 # resource "netris_allocation" "my-allocation-loopback" {
 #   name = "my-allocation-loopback"
 #   prefix = "198.51.100.0/24"
 #   tenantid = data.netris_tenant.admin.id
-#   depends_on = [
-#     netris_site.santa-clara,
-#   ]
 # }
 
 # resource "netris_allocation" "my-allocation-common" {
 #   name = "my-allocation-common"
 #   prefix = "203.0.113.0/24"
 #   tenantid = data.netris_tenant.admin.id
-#   depends_on = [
-#     netris_site.santa-clara,
-#   ]
 # }
