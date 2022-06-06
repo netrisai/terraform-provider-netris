149b80b9720dc6e5b614a9a74d4003f4d3b4ff1a
Gor Poghosyan <gor@netris.ai>
2022-06-06 14:58:31 +0400

    update site example

--- examples/site_example.tf
+++ examples/site_example.tf
@@ -1,9 +1,14 @@
 resource "netris_site" "santa-clara" {
-  name              = "Santa Clara"
-  publicasn         = 65001
-  rohasn            = 65502
-  vmasn             = 65503
-  rohroutingprofile = "default"
-  sitemesh          = "hub"
-  acldefaultpolicy  = "permit"
+  name                 = "Santa Clara"
+  publicasn            = 65001
+  rohasn               = 65502
+  vmasn                = 65503
+  rohroutingprofile    = "default"
+  sitemesh             = "hub"
+  acldefaultpolicy     = "permit"
+  # switchfabric         = "equinix_metal"
+  # # vlanrange            = "2-3999"
+  # equinixprojectid     = "xxxx"
+  # equinixprojectapikey = "yyyy"
+  # equinixlocation      = "sv"
 }
