663f8b5641f6893139d7047ccf5ba4ebc8353691
Gor Poghosyan <gor@netris.ai>
2023-07-17 16:30:14 +0400

    feat: added vpcid support in route resource

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/hashicorp/terraform-plugin-sdk v1.17.2
-	github.com/netrisai/netriswebapi v0.0.0-20230704115706-ac0216c555fc
+	github.com/netrisai/netriswebapi v0.0.0-20230717120630-c1c8101e30c4
 
 )
 
--- netris/route/route.go
+++ netris/route/route.go
@@ -53,6 +53,12 @@ func Resource() *schema.Resource {
 				Type:        schema.TypeInt,
 				Description: "The site ID where the current route belongs",
 			},
+			"vpcid": {
+				ForceNew:    true,
+				Optional:    true,
+				Type:        schema.TypeInt,
+				Description: "ID of VPC. If not specified, the route will be created in the VPC marked as a default.",
+			},
 			"state": {
 				Default:     "enabled",
 				Optional:    true,
@@ -85,6 +91,7 @@ func resourceCreate(d *schema.ResourceData, m interface{}) error {
 
 	hwIds := []int{}
 	hws := d.Get("hwids").([]interface{})
+	vpcid := d.Get("vpcid").(int)
 
 	for _, v := range hws {
 		hwIds = append(hwIds, v.(int))
@@ -97,6 +104,10 @@ func resourceCreate(d *schema.ResourceData, m interface{}) error {
 		SiteID:      d.Get("siteid").(int),
 		StateStatus: d.Get("state").(string),
 		Switches:    hwIds,
+	}
+
+	if vpcid > 0 {
+		routeAdd.Vpc = &route.IDName{ID: vpcid}
 	}
 
 	js, _ := json.Marshal(routeAdd)
@@ -155,6 +166,7 @@ func resourceRead(d *schema.ResourceData, m interface{}) error {
 			break
 		}
 	}
+	currentVpcId := d.Get("vpcid").(int)
 
 	if route == nil {
 		return fmt.Errorf("Coudn't find route by id %d", id)
@@ -180,6 +192,13 @@ func resourceRead(d *schema.ResourceData, m interface{}) error {
 	err = d.Set("state", route.State)
 	if err != nil {
 		return err
+	}
+
+	if currentVpcId > 0 {
+		err = d.Set("vpcid", route.Vpc.ID)
+		if err != nil {
+			return err
+		}
 	}
 
 	hwids := []int{}
