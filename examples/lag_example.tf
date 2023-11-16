e927093f53b360c025b8e589ebedf9538c7b4dcf
Gor Poghosyan <gor@netris.ai>
2023-11-15 20:11:20 -0800

    update lag example

--- examples/lag_example.tf
+++ examples/lag_example.tf
@@ -1,14 +1,17 @@
-resource "netris_lag" "lag-terraform-switch-01" {
-  description = "Terraform LAG"
+resource "netris_lag" "lag1-switch-01" {
+  description = "my lag1"
   tenantid    = data.netris_tenant.admin.id
-  mtu         = 9000                           # Optional
-  lacp        = "off"                          # Optional
-  extension = {                                # Optional
-     extensionname = "aggsw1"
-     vlanrange = "10-13"
-  }
-  members = [                                  # at least one member port is required
-    "swp8@my-switch01",
-    "swp9@my-switch01",
+  mtu         = 9008                             # Optional
+  # lacp        = "on"                           # Optional. Possible values: 'off' or 'on'
+  # extension = {                                # Optional
+  #    extensionname = "ext1"
+  #    vlanrange = "10-20"
+  # }
+  members = [                                    # at least one member port is required
+    "swp11@my-switch01",
+    "swp12@my-switch01",
+  ]
+  depends_on = [
+    netris_switch.my-switch01,
   ]
 }
