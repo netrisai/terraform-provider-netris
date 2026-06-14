16da1cf55c576d25575298a849d07d2f24c406a9
Gor Poghosyan <gor@netris.ai>
2026-06-14 12:29:37 +0400

    update example

--- examples/terraform.tf
+++ examples/terraform.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "netris" {
-  address  = "https://ntrs-acs-rc.8399ca9b.nip.io" # overwrite env: NETRIS_ADDRESS
+  address  = "http://netris.example.com"           # overwrite env: NETRIS_ADDRESS
   login    = "netris"                              # overwrite env: NETRIS_LOGIN
   password = "newNet0ps"                           # overwrite env: NETRIS_PASSWORD
 }
